19099b7c69cf2bb3657cd5906ca78c551b2b6152
Kai19 <ngyankai@hotmail.com>
2017-07-05 03:10:19 +0000

    made a function that generations a random 6 char alphanumeric string and a new page that can post back to server urlDatabase

--- express_server.js
+++ express_server.js
@@ -2,6 +2,17 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+function generateRandomString() {
+  const length = 6;
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let str = "";
+
+  for (let i = 0; i < length; i++) {
+      str = str + chars[Math.floor(Math.random() * chars.length)];
+  }
+  return str;
+}
+
 app.set("view engine", "ejs")
 
 var urlDatabase = {
@@ -17,10 +28,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", {urls: urlDatabase});
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   res.render("urls_show", { shortURL:req.params.id,
                             longURL: urlDatabase[req.params.id]});
-                          });
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
