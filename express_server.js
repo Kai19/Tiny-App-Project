96b873da9648904a931076aafae3a4b33d123e85
Kai19 <ngyankai@hotmail.com>
2017-07-05 04:22:25 +0000

    added redirect after form submission, and redirect for short url to long url and validated

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 function generateRandomString() {
   const length = 6;
   const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -32,18 +35,29 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if(longURL){
+    res.redirect(longURL);
+  }else{
+    res.end("Not found");
+  }
+});
+
 app.get("/urls/:id", (req, res) => {
   res.render("urls_show", { shortURL:req.params.id,
                             longURL: urlDatabase[req.params.id]});
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randString = generateRandomString();
+
+  urlDatabase[randString] = req.body.longURL;
+  console.log(urlDatabase);
+  console.log(req.body);
+  res.redirect(`/urls/${randString}`);
 });
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
