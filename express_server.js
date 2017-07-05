0a4b0ed8e7a2b39188f4f0e1d1ae143657ef2a8f
Kai19 <ngyankai@hotmail.com>
2017-07-05 01:01:12 +0000

    tinyApp server setup with express and made 2 ejs files and included partials for both

---
+++ express_server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  res.render("urls_index", {urls: urlDatabase});
+});
+
+app.get("/urls/:id", (req, res) => {
+  res.render("urls_show", { shortURL: urlDatabase[req.params.id]});
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
